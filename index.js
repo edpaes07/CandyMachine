3f8f5030025c4b9d79b785ec12cef3f1cb6a587f
Edmilson Bispo Paes dos Santos <edmilson.santos@iteris.com.br>
2021-03-28 19:09:30 -0300

    Criacao Inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+/* alert("Recomendamos a utilização da resolução 1366x768"); */
+
+let texto = document.getElementById("texto");
+
+function next(){
+    if(texto.innerHTML === "Fred curte muito os doces da cat candy machine"){
+         texto.innerHTML = "Poderia por favor comprar doces para o Fred?"
+    }else{
+        texto.innerHTML = "Fred curte muito os doces da cat candy machine";
+    }
+}
+
+
+let botoes = document.querySelectorAll("#doce");
+let precos =  document.querySelectorAll("#precos");
+
